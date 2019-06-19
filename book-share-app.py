13839b9d03ebeeb7f148732ed6a423d9a1edc2a9
amyklopfen <50849294+amyklopfen@users.noreply.github.com>
2019-06-19 19:59:33 -0400

    setup email function in app for contacting lender

--- book-share-app.py
+++ book-share-app.py
@@ -1,3 +1,19 @@
+import os
+from dotenv import load_dotenv
+import sendgrid
+from sendgrid.helpers.mail import * # info drawn from send notification exercise.
+
+load_dotenv()
+
+import pprint
+
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
+MY_EMAIL_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
+
+sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
+
+#lender_list = [amyklopfen, dougschulte, sarahlazun]
+
 amyklopfen_library = [{"author":"Sanderson", "title":"The Way of Kings", "ISBN": 9780765365279, "genre":"sci-fi"}, 
 {"author":"Tolkein", "title":"The Hobbit", "ISBN": 9780345339683, "genre":"fantasy"},
 {"author":"Sanderson", "title":"The Hero of the Ages", "ISBN": 9780765377159, "genre":"sci-fi"},
@@ -33,8 +49,21 @@ sarahlazun_library = [{"author":"author", "title":"title", "ISBN": "ISBN", "genr
 
 user_libraries = [amyklopfen_library, sarahlazun_library, dougschulte_library]
 
+#welcome user, give instructions on how to use library
+print("Welcome to bookshare, a community of mini-libraries. Search for a title, author, or genre to get started.")
+
 #list of user emails? 
 #set up profile - append email to user email list
+my_user_name = input("Enter a username to get started: ")
+
+library_create = input("Would you like to set up your own library?")
+
+if library_create == "yes":
+    user_input = input("Great, add books from your shelf to get started: ")
+    list_of_books = user_input.split()
+
+#borrowing
+print("Input a list of books you would like to lend to gest started")
 
 #background
     #install packages
@@ -62,3 +91,37 @@ user_libraries = [amyklopfen_library, sarahlazun_library, dougschulte_library]
 #give user ability to send email request to book buddy 
     #sendgrid API
     #set up message function to other user - email?
+    #set up existing user emails in .env, assign them each a unique variable by username
+    #user selects amyklopfen_library
+    #.env has amyklopfen = ak6266@stern.nyu.edu 
+    #code has request_email = input("Please enter the user you would like to borrow from.")
+    #sendgrid code has request_email embedded
+
+selected_user = input("Please enter the name of the user you would like to borrow from: ")
+selected_user = os.environ.get(selected_user, "OOPS, please set env var called 'selected_user'")
+book_request = input("Please enter the name of the book you would like to borrow: ")
+
+
+from_email = Email(MY_EMAIL_ADDRESS)
+to_email = Email(selected_user)
+subject = "You have a new checkout request"
+message_text = "Amy" + "would like to borrow your copy of" + "book name" + "please reach out to them at" + "user email" + "to coordinate pickup"
+content = Content("text/plain", message_text)
+mail = Mail(from_email, subject, to_email, content)
+
+
+response = sg.client.mail.send.post(request_body=mail.get())
+
+
+
+pp = pprint.PrettyPrinter(indent=4)
+
+print("----------------------")
+print("EMAIL")
+print("----------------------")
+print("RESPONSE: ", type(response))
+print("STATUS:", response.status_code) 
+print("HEADERS:")
+pp.pprint(dict(response.headers))
+print("BODY:")
+print(response.body) 
